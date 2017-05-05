647a89fd4cff03bd7d93e19b5ee1d3f17c0e8024
i-rochiño <irocho.profe@gmail.com>
2017-05-05 11:17:44 +0200

    Update and rename 12_redirecionar.sh to 12_redirecionar.cmd

---
+++ 12_redirecionar.cmd
@@ -0,0 +1,13 @@
+#!
+###########################################################
+#
+# Redirecionamento
+#
+# autor:
+###########################################################
+
+# fabrica un ficheiro que o seu contido sexa a saída de dir
+
+# mostra no terminal o contido dese ficheiro
+
+# fabrica un ficheiro que o seu contido sexa a páxina da axuda de dir
