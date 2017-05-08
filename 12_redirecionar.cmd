84f0e82aff59498bc25576cd5800c03a9dafff5b
i-rochiño <irocho.profe@gmail.com>
2017-05-08 13:31:01 +0200

    Update 12_redirecionar.cmd

--- 12_redirecionar.cmd
+++ 12_redirecionar.cmd
@@ -1,13 +1,15 @@
-#!
-###########################################################
-#
-# Redirecionamento
-#
-# autor:
-###########################################################
 
-# fabrica un ficheiro que o seu contido sexa a saída de dir
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# mostra no terminal o contido dese ficheiro
+::
+:: Redirecionamento
+::
+:: autor:
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# fabrica un ficheiro que o seu contido sexa a páxina da axuda de dir
+
+:: fabrica un ficheiro que o seu contido sexa a saída de dir
+
+:: mostra no terminal o contido dese ficheiro
+
+:: fabrica un ficheiro que o seu contido sexa a páxina da axuda de dir
