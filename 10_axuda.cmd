5fad3d8b146cdca42df666355843efb9e0a742b6
i-rochiño <irocho.profe@gmail.com>
2017-05-08 13:31:52 +0200

    Update and rename axuda.cmd to 10_axuda.cmd

---
+++ 10_axuda.cmd
@@ -0,0 +1,12 @@
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::
+:: Móstralle ó usuario o manual do comando que pide 
+:: autor:
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+:: pregunta ó usuario cal é o comando que lle interesa
+
+:: mostra a páxina da axuda dese comando
+
+:: fabrica un ficheiro coa información que saiu no terminal
